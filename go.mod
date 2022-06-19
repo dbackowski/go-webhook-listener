42d89b480e272e20ec885161522973b09c0ff574
Damian Baćkowski <damianbackowski@gmail.com>
2022-06-19 15:53:32 +0200

    Initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module webhook-listener
+
+go 1.16
+
+require github.com/Jeffail/gabs/v2 v2.6.1 // indirect
---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+
+	"github.com/Jeffail/gabs/v2"
+)
+
+var port int = 8080
+
+func handleWebhook(w http.ResponseWriter, r *http.Request) {
+	printHeaders(r)
+	printBody(r)
+}
+
+func printHeaders(r *http.Request) {
+	fmt.Println("HEADERS:")
+	fmt.Println("--------")
+	for name, values := range r.Header {
+		for _, value := range values {
+			fmt.Println(name, value)
+		}
+	}
+}
+
+func printBody(r *http.Request) {
+	body, _ := ioutil.ReadAll(r.Body)
+	jsonParsed, err := gabs.ParseJSON(body)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("BODY:")
+	fmt.Println("-----")
+	fmt.Println(jsonParsed.StringIndent("", "  "))
+}
+
+func main() {
+	log.Println(fmt.Sprintf("server started on port: %d", port))
+	http.HandleFunc("/", handleWebhook)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), nil))
+}
