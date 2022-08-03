1870a3e98430dcd5e1b1c5a3a05f95c035272efc
Damian Baćkowski <damianbackowski@gmail.com>
2022-08-03 18:36:06 +0200

    Fix tests

--- webhook_listener_test.go
+++ webhook_listener_test.go
@@ -12,10 +12,7 @@ import (
 
 func TestHandleWebhook(t *testing.T) {
 	t.Run("for valid json payload it returns propers headers and payload", func(t *testing.T) {
-		var jsonData = []byte(`{
-			"name": "morpheus",
-			"job": "leader"
-		}`)
+		var jsonData = []byte(`{ "name": "morpheus", "job": "leader" }`)
 		request, _ := http.NewRequest(http.MethodPost, "/", bytes.NewBuffer(jsonData))
 		request.Header.Set("Content-Type", "application/json; charset=UTF-8")
 		response := httptest.NewRecorder()
@@ -33,7 +30,7 @@ func TestHandleWebhook(t *testing.T) {
 		fmt.Println(string(out))
 
 		got := string(out)
-		want := "\x1b[32mHEADERS:\x1b[0m\n--------\nContent-Type: application/json; charset=UTF-8\n\n\x1b[32mBODY:\x1b[0m\n-----\n{\n  \"job\": \"leader\",\n  \"name\": \"morpheus\"\n}\n"
+		want := "\x1b[32mHEADERS:\x1b[0m\n--------\nContent-Type: application/json; charset=UTF-8\n\n\x1b[32mBODY:\x1b[0m\n-----\n{ \"name\": \"morpheus\", \"job\": \"leader\" }\n"
 
 		if got != want {
 			t.Errorf("got %q, want %q", got, want)
@@ -41,9 +38,7 @@ func TestHandleWebhook(t *testing.T) {
 	})
 
 	t.Run("for invalid json payload it returns propers headers and payload", func(t *testing.T) {
-		var jsonData = []byte(`{
-			"name": "morpheus",
-		}`)
+		var jsonData = []byte(`{ "name": "morpheus", }`)
 		request, _ := http.NewRequest(http.MethodPost, "/", bytes.NewBuffer(jsonData))
 		request.Header.Set("Content-Type", "application/json; charset=UTF-8")
 		response := httptest.NewRecorder()
@@ -61,7 +56,7 @@ func TestHandleWebhook(t *testing.T) {
 		fmt.Println(string(out))
 
 		got := string(out)
-		want := "\x1b[32mHEADERS:\x1b[0m\n--------\nContent-Type: application/json; charset=UTF-8\n\n\x1b[32mBODY:\x1b[0m\n-----\n\x1b[31mCannot decode JSON payload!\x1b[0m\n{\n\t\t\t\"name\": \"morpheus\",\n\t\t}\n"
+		want := "\x1b[32mHEADERS:\x1b[0m\n--------\nContent-Type: application/json; charset=UTF-8\n\n\x1b[31mCannot decode JSON payload!\x1b[0m\n{ \"name\": \"morpheus\", }\ninvalid character '}' looking for beginning of object key string\n"
 
 		if got != want {
 			t.Errorf("got %q, want %q", got, want)
