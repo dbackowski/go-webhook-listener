9ef18c6581badb9fc178c2e2a5cca4cf8fc82801
Damian Baćkowski <damianbackowski@gmail.com>
2022-08-03 18:25:06 +0200

    Remove gabs package

--- go.mod
+++ go.mod
@@ -2,7 +2,4 @@ module webhook-listener
 
 go 1.16
 
-require (
-	github.com/Jeffail/gabs/v2 v2.6.1 // indirect
-	github.com/dbackowski/colors v0.0.0-20220621083935-7b2925c0d653 // indirect
-)
+require github.com/dbackowski/colors v0.0.0-20220717142508-6be69bb6c310
--- handle_webhook.go
+++ handle_webhook.go
@@ -1,11 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 
-	"github.com/Jeffail/gabs/v2"
 	"github.com/dbackowski/colors"
 )
 
@@ -27,17 +27,19 @@ func printHeaders(r *http.Request) {
 
 func printBody(w http.ResponseWriter, r *http.Request) {
 	body, _ := ioutil.ReadAll(r.Body)
-	jsonParsed, err := gabs.ParseJSON(body)
+	var jsonRawMessage json.RawMessage
 
-	fmt.Println(colors.Colorize("BODY:", colors.FgGreen))
-	fmt.Println("-----")
+	err := json.Unmarshal(body, &jsonRawMessage)
 
 	if err != nil {
 		fmt.Println(colors.Colorize("Cannot decode JSON payload!", colors.FgRed))
 		fmt.Println(string(body))
+		fmt.Println(err)
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
 
-	fmt.Println(jsonParsed.StringIndent("", "  "))
+	fmt.Println(colors.Colorize("BODY:", colors.FgGreen))
+	fmt.Println("-----")
+	fmt.Println(string(jsonRawMessage))
 }
