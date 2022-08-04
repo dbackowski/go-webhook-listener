54dd121de2526f3b9aa4ad6f213be41977e942f7
Damian Baćkowski <damianbackowski@gmail.com>
2022-08-04 17:39:26 +0200

    Refactor tests

--- webhook_listener_test.go
+++ webhook_listener_test.go
@@ -2,13 +2,26 @@ package main
 
 import (
 	"bytes"
-	"fmt"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"os"
 	"testing"
 )
+
+func captureHandleWebhookConsoleOutput(response http.ResponseWriter, request *http.Request) string {
+	rescueStdout := os.Stdout
+	r, w, _ := os.Pipe()
+	os.Stdout = w
+
+	HandleWebhook(response, request)
+
+	w.Close()
+	out, _ := ioutil.ReadAll(r)
+	os.Stdout = rescueStdout
+
+	return string(out)
+}
 
 func TestHandleWebhook(t *testing.T) {
 	t.Run("for valid json payload it returns propers headers and payload", func(t *testing.T) {
@@ -17,23 +30,18 @@ func TestHandleWebhook(t *testing.T) {
 		request.Header.Set("Content-Type", "application/json; charset=UTF-8")
 		response := httptest.NewRecorder()
 
-		rescueStdout := os.Stdout
-		r, w, _ := os.Pipe()
-		os.Stdout = w
-
-		HandleWebhook(response, request)
-
-		w.Close()
-		out, _ := ioutil.ReadAll(r)
-		os.Stdout = rescueStdout
-
-		fmt.Println(string(out))
-
-		got := string(out)
+		got := captureHandleWebhookConsoleOutput(response, request)
 		want := "\x1b[32mHEADERS:\x1b[0m\n--------\nContent-Type: application/json; charset=UTF-8\n\n\x1b[32mBODY:\x1b[0m\n-----\n{ \"name\": \"morpheus\", \"job\": \"leader\" }\n"
 
 		if got != want {
 			t.Errorf("got %q, want %q", got, want)
+		}
+
+		gotStatusCode := response.Result().StatusCode
+		wantStatusCode := http.StatusOK
+
+		if gotStatusCode != wantStatusCode {
+			t.Errorf("got %v, want %v", gotStatusCode, wantStatusCode)
 		}
 	})
 
@@ -43,23 +51,18 @@ func TestHandleWebhook(t *testing.T) {
 		request.Header.Set("Content-Type", "application/json; charset=UTF-8")
 		response := httptest.NewRecorder()
 
-		rescueStdout := os.Stdout
-		r, w, _ := os.Pipe()
-		os.Stdout = w
-
-		HandleWebhook(response, request)
-
-		w.Close()
-		out, _ := ioutil.ReadAll(r)
-		os.Stdout = rescueStdout
-
-		fmt.Println(string(out))
-
-		got := string(out)
+		got := captureHandleWebhookConsoleOutput(response, request)
 		want := "\x1b[32mHEADERS:\x1b[0m\n--------\nContent-Type: application/json; charset=UTF-8\n\n\x1b[31mCannot decode JSON payload!\x1b[0m\n{ \"name\": \"morpheus\", }\ninvalid character '}' looking for beginning of object key string\n"
 
 		if got != want {
 			t.Errorf("got %q, want %q", got, want)
+		}
+
+		gotStatusCode := response.Result().StatusCode
+		wantStatusCode := http.StatusBadRequest
+
+		if gotStatusCode != wantStatusCode {
+			t.Errorf("got %v, want %v", gotStatusCode, wantStatusCode)
 		}
 	})
 }
