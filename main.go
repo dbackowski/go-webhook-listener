4f1989fedad822cc47c709e902a93a5678b4bc06
Damian Baćkowski <damian.backowski@chargify.com>
2023-02-27 14:55:57 +0100

    Add command line option to pass the port number

--- main.go
+++ main.go
@@ -1,14 +1,17 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
 )
 
-var port int = 8080
-
 func main() {
+	portPtr := flag.Int("port", 8080, "port number")
+	flag.Parse()
+	port := *portPtr
+
 	log.Println(fmt.Sprintf("server started on port: %d", port))
 	http.HandleFunc("/", HandleWebhook)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), nil))
